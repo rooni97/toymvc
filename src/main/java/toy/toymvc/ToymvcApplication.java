537fd244d52bd1ad96d6861b0e2d5f014c39d667
cy <cy@cyui-MacBookAir.local>
2022-08-23 16:19:28 +0900

    초기 세팅

---
+++ src/main/java/toy/toymvc/ToymvcApplication.java
@@ -0,0 +1,13 @@
+package toy.toymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToymvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToymvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/toymvc/ToymvcApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.toymvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToymvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
